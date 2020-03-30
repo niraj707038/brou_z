a7ce2a5aaf90eec9778c7cbb731cf221846f37aa
niraj707038 <niraj707038@gamil.com>
2020-03-30 20:22:55 +0530

    inal

--- app/src/main/java/com/ranjan/myapps/Model/SubscriptionModel.java
+++
@@ -1,7 +0,0 @@
-package com.ranjan.myapps.Model;
-
-public class SubscriptionModel {
-
-    public String message;
-    public String statu;
-}
--- app/src/main/java/com/ranjan/myapps/webservice/OnResponseInterface.java
+++
@@ -1,6 +0,0 @@
-package com.ranjan.myapps.webservice;
-
-public interface OnResponseInterface {
-    void onApiResponse(Object response);
-    void onApiFailure(String message);
-}
